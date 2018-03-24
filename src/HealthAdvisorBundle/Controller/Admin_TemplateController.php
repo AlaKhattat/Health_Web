4ea9d35d235b16a220fad6a02a53af80032babe1
khattout <alaeddine.khattat@esprit.tn>
2018-03-24 13:05:28 +0100

    new commit

--- src/HealthAdvisorBundle/Controller/Admin_TemplateController.php
+++ src/HealthAdvisorBundle/Controller/Admin_TemplateController.php
@@ -3,6 +3,7 @@
 namespace HealthAdvisorBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class Admin_TemplateController extends Controller
 {
@@ -18,6 +19,17 @@ class Admin_TemplateController extends Controller
         $reposit=$this->getDoctrine()->getRepository("HealthAdvisorBundle:Utilisateur");
         $list=$reposit->findAll();
         return $this->render('@HealthAdvisor/Admin_Template/users_profile.html.twig',array('users'=>$list));
+    }
+    public function supprimer_UserAction(Request $request)
+    {
+        $lot=$this->getDoctrine()
+            ->getRepository('HealthAdvisorBundle:Utilisateur')
+            ->find($request->get('id'));
+        $this->getDoctrine()
+            ->getManager()
+            ->remove($lot);
+        $this->getDoctrine()->getManager()->flush();
+        return $this->redirectToRoute('admin_users');
     }
 
 }
