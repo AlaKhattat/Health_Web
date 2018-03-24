b103757a6b917629b66ede192d723c321ca4e162
khattout <alaeddine.khattat@esprit.tn>
2018-03-24 17:29:44 +0100

    new commit

--- src/HealthAdvisorBundle/Controller/Admin_TemplateController.php
+++ src/HealthAdvisorBundle/Controller/Admin_TemplateController.php
@@ -2,8 +2,10 @@
 
 namespace HealthAdvisorBundle\Controller;
 
+use FOS\UserBundle\Model\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class Admin_TemplateController extends Controller
 {
@@ -16,18 +18,22 @@ class Admin_TemplateController extends Controller
 
     public function afficher_UsersAction()
     {
+        $user = $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
         $reposit=$this->getDoctrine()->getRepository("HealthAdvisorBundle:Utilisateur");
         $list=$reposit->findAll();
-        return $this->render('@HealthAdvisor/Admin_Template/users_profile.html.twig',array('users'=>$list));
+        return $this->render('@HealthAdvisor/Admin_Template/users_profile.html.twig',array('users'=>$list,'user'=>$user));
     }
     public function supprimer_UserAction(Request $request)
     {
-        $lot=$this->getDoctrine()
+        $user=$this->getDoctrine()
             ->getRepository('HealthAdvisorBundle:Utilisateur')
             ->find($request->get('id'));
         $this->getDoctrine()
             ->getManager()
-            ->remove($lot);
+            ->remove($user);
         $this->getDoctrine()->getManager()->flush();
         return $this->redirectToRoute('admin_users');
     }
---
+++ src/HealthAdvisorBundle/Controller/UtilisateurController.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace HealthAdvisorBundle\Controller;
+
+use FOS\UserBundle\Model\UserInterface;
+use HealthAdvisorBundle\Entity\Utilisateur;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+/**
+ * Utilisateur controller.
+ *
+ * @Route("utilisateur")
+ */
+class UtilisateurController extends Controller
+{
+    /**
+     * Lists all utilisateur entities.
+     *
+     * @Route("/", name="utilisateur_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $user = $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+        $em = $this->getDoctrine()->getManager();
+
+        $utilisateurs = $em->getRepository('HealthAdvisorBundle:Utilisateur')->find($user->getId());
+        $patient =$em->getRepository('HealthAdvisorBundle:Patient')->findOneBy(array('cinUser'=>$utilisateurs->getId()));
+        $medecin =$em->getRepository('HealthAdvisorBundle:Medecin')->findOneBy(array('login'=>$patient->getLogin()));
+        return $this->render('utilisateur/index.html.twig', array(
+            'utilisateur' => $utilisateurs,'medecin'=>$medecin
+        ));
+
+    }
+
+    /**
+     * Creates a new utilisateur entity.
+     *
+     * @Route("/new", name="utilisateur_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $utilisateur = new Utilisateur();
+        $form = $this->createForm('HealthAdvisorBundle\Form\UtilisateurType', $utilisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($utilisateur);
+            $em->flush();
+
+            return $this->redirectToRoute('utilisateur_show', array('id' => $utilisateur->getId()));
+        }
+
+        return $this->render('utilisateur/new.html.twig', array(
+            'utilisateur' => $utilisateur,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a utilisateur entity.
+     *
+     * @Route("/{id}", name="utilisateur_show")
+     * @Method("GET")
+     */
+    public function showAction(Utilisateur $utilisateur)
+    {
+        $deleteForm = $this->createDeleteForm($utilisateur);
+
+        return $this->render('utilisateur/show.html.twig', array(
+            'utilisateur' => $utilisateur,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing utilisateur entity.
+     *
+     * @Route("/{id}/edit", name="utilisateur_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Utilisateur $utilisateur)
+    {
+        $deleteForm = $this->createDeleteForm($utilisateur);
+        $editForm = $this->createForm('HealthAdvisorBundle\Form\UtilisateurType', $utilisateur);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('utilisateur_edit', array('id' => $utilisateur->getId()));
+        }
+
+        return $this->render('utilisateur/edit.html.twig', array(
+            'utilisateur' => $utilisateur,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a utilisateur entity.
+     *
+     * @Route("/{id}", name="utilisateur_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Utilisateur $utilisateur)
+    {
+        $form = $this->createDeleteForm($utilisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($utilisateur);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('utilisateur_index');
+    }
+
+    /**
+     * Creates a form to delete a utilisateur entity.
+     *
+     * @param Utilisateur $utilisateur The utilisateur entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Utilisateur $utilisateur)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('utilisateur_delete', array('id' => $utilisateur->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
