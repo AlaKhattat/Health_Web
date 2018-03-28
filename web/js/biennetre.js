f4638d863d4bc192539c052dafc382ce8dd4cea6
Brehima Coulibaly <brehima.coulibaly@esprit.tn>
2018-03-28 16:41:52 +0100

    ajout control  de  saisie dans la tache calcul IMC

--- web/js/biennetre.js
+++ web/js/biennetre.js
@@ -1,99 +1,99 @@
-function verifPoid($poidVerif) {
+function verifPoid(poidVerif,dive) {
 
-    var poids=  Number($poidVerif);
+    var poids=  Number(poidVerif);
     if(poids.toLocaleString()==="NaN") {
-        $(".divPoids").removeClass("has-succes").addClass("has-error");
-        $(".spanPoids").css("display","block").addClass("glyphicon glyphicon-remove ").slideDown("slow");
-        $(".helpPoids").css("display","block").slideDown("slow").text("Veullez saisir un nombre correcte");
+       console.log( $(dive).find("span")[0]);
+        $(dive).removeClass("has-succes").addClass("has-error");
+        $(dive).find($(dive).find("span")[0]).css("display","block").addClass("glyphicon glyphicon-remove ").slideDown("slow");
+        $(dive).find($(dive).find("span")[1]).css("display","block").slideDown("slow").text("Veullez saisir un nombre correcte");
         return false;
     }
     else
     {
         if((poids<=0) || (poids>=1 && poids <=10) || (poids > 560)) {
-            $(".divPoids").removeClass("has-succes").addClass("has-error");
-            $(".spanPoids").css("display","block").addClass("glyphicon glyphicon-remove ").slideDown("slow");
-            $(".helpPoids").css("display","block").slideDown("slow").text("Veullez saisir un poid valide");
+            $(dive).removeClass("has-succes").addClass("has-error");
+            $(dive).find($(dive).find("span")[0]).css("display","block").addClass("glyphicon glyphicon-remove ").slideDown("slow");
+            $(dive).find($(dive).find("span")[1]).css("display","block").slideDown("slow").text("Veullez saisir un poid valide");
             return false;
 
         }
         else if((poids > 10) &&(poids <= 560))
         {
-            $(".divPoids").removeClass("has-error").addClass("has-success");
-            $(".spanPoids").css("display","block").removeClass("glyphicon glyphicon-remove").addClass("glyphicon glyphicon-ok ").slideDown("slow");
-            $(".helpPoids").css("display","none").slideUp("slow");
+            $(dive).removeClass("has-error").addClass("has-success");
+            $(dive).find($(dive).find("span")[0]).css("display","block").removeClass("glyphicon glyphicon-remove").addClass("glyphicon glyphicon-ok ").slideDown("slow");
+            $(dive).find($(dive).find("span")[1]).css("display","none").slideUp("slow");
             return true;
         }
     }
     return false;
 }
-function verifTaille(tailleVerif) {
+function verifTaille(tailleVerif,diveTaille) {
     var taille=  Number(tailleVerif);
-
+      console.log(taille);
     if(taille.toLocaleString()==="NaN") {
-        $(".divTaille").removeClass("has-succes").addClass("has-error");
-        $(".spanTaille").css("display","block").addClass("glyphicon glyphicon-remove ").slideDown("slow");
-        $(".helpTaille").css("display","block").slideDown("slow").text("Veullez saisir un nombre correcte");
+        $(diveTaille).removeClass("has-succes").addClass("has-error");
+        $(diveTaille).find($(".divTaille").find("span")[0]).css("display","block").addClass("glyphicon glyphicon-remove ").slideDown("slow");
+        $(diveTaille).find($(".divTaille").find("span")[1]).css("display","block").slideDown("slow").text("Veullez saisir un nombre correcte");
         return false;
     }
     else
     {
         if((taille<=0) || (taille>=1 && taille <=57) || (taille > 280)) {
-            $(".divTaille").removeClass("has-succes").addClass("has-error");
-            $(".spanTaille").css("display","block").addClass("glyphicon glyphicon-remove ").slideDown("slow");
-            $(".helpTaille").css("display","block").slideDown("slow").text("Veullez saisir votre taille en cm");
+            $(diveTaille).removeClass("has-succes").addClass("has-error");
+            $(diveTaille).find($(diveTaille).find("span")[0]).css("display","block").addClass("glyphicon glyphicon-remove ").slideDown("slow");
+            $(diveTaille).find($(diveTaille).find("span")[1]).css("display","block").slideDown("slow").text("Veullez saisir votre taille en cm");
             return false;
 
         }
         else
         {
-            $(".divTaille").removeClass("has-error").addClass("has-success");
-            $(".spanTaille").css("display","block").removeClass("glyphicon glyphicon-remove").addClass("glyphicon glyphicon-ok ").slideDown("slow");
-            $(".helpTaille").css("display","none").slideUp("slow");
+            $(diveTaille).removeClass("has-error").addClass("has-success");
+            $(diveTaille).find($(".divTaille").find("span")[0]).css("display","block").removeClass("glyphicon glyphicon-remove").addClass("glyphicon glyphicon-ok ").slideDown("slow");
+            $(diveTaille).find($(".divTaille").find("span")[1]).css("display","none").slideUp("slow");
             return true;
         }
     }
     return false;
 }
-function verifSexe(sexeVerif) {
+function verifSexe(sexeVerif,dive) {
     var sexe=  Number(sexeVerif);
-
     if(sexe.toLocaleString()==="Sexe") {
-        $(".divSexe").removeClass("has-succes").addClass("has-error");
-        $(".helpSexe").css("display","block").slideDown("slow").text("Veullez choisir un Homme/Femme");
+        $(dive).removeClass("has-succes").addClass("has-error");
+        $(dive).find("help-block").css("display","block").slideDown("slow").text("Veullez choisir un Homme/Femme");
         return false;
     }
     else
     {
-        $(".divSexe").removeClass("has-error").addClass("has-success");
-        $(".helpSexe").css("display","none").slideUp("slow");
+        $(dive).removeClass("has-error").addClass("has-success");
+        $(dive).find("help-block").css("display","none").slideUp("slow");
         return true;
     }
     return false;
 }
 
-function verifAge(ageVerif) {
+function verifAge(ageVerif,dive) {
     var age=  Number(ageVerif);
 
     if(age.toLocaleString()==="NaN") {
-        $(".divAge").removeClass("has-succes").addClass("has-error");
-        $(".spanAge").css("display","block").addClass("glyphicon glyphicon-remove ").slideDown("slow");
-        $(".helpAge").css("display","block").slideDown("slow").text("Veullez saisir un nombre correcte");
+        $(dive).removeClass("has-succes").addClass("has-error");
+        $(dive).find($(".divAge").find("span")[0]).css("display","block").addClass("glyphicon glyphicon-remove ").slideDown("slow");
+        $(dive).find($(".divAge").find("span")[1]).css("display","block").slideDown("slow").text("Veullez saisir un nombre correcte");
         return false;
     }
     else
     {
         if((age<=0)  || (age > 100)) {
-            $(".divAge").removeClass("has-succes").addClass("has-error");
-            $(".spanAge").css("display","block").addClass("glyphicon glyphicon-remove ").slideDown("slow");
-            $(".helpAge").css("display","block").slideDown("slow").text("Veullez saisir votre age correctement");
+            $(dive).removeClass("has-succes").addClass("has-error");
+            $(dive).find($(".divAge").find("span")[0]).css("display","block").addClass("glyphicon glyphicon-remove ").slideDown("slow");
+            $(dive).find($(".divAge").find("span")[1]).css("display","block").slideDown("slow").text("Veullez saisir votre age correctement");
             return false;
 
         }
         else
         {
-            $(".divAge").removeClass("has-error").addClass("has-success");
-            $(".spanAge").css("display","block").removeClass("glyphicon glyphicon-remove").addClass("glyphicon glyphicon-ok ").slideDown("slow");
-            $(".helpAge").css("display","none").slideUp("slow");
+            $(dive).removeClass("has-error").addClass("has-success");
+            $(dive).find($(".divAge").find("span")[0]).css("display","block").removeClass("glyphicon glyphicon-remove").addClass("glyphicon glyphicon-ok ").slideDown("slow");
+            $(dive).find($(".divAge").find("span")[1]).css("display","none").slideUp("slow");
             return true;
         }
     }
