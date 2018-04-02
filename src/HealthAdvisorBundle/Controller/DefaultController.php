47bc5f88f7c7fcd61251d771ffef6886f0452860
Firassov <firas.mrad@esprit.tn>
2018-04-02 13:51:23 +0100

    Analyse Symptomes almost done

--- src/HealthAdvisorBundle/Controller/DefaultController.php
+++ src/HealthAdvisorBundle/Controller/DefaultController.php
@@ -2,7 +2,12 @@
 
 namespace HealthAdvisorBundle\Controller;
 
+use HealthAdvisorBundle\Entity\BodyParts;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class DefaultController extends Controller
 {
@@ -14,6 +19,44 @@ class DefaultController extends Controller
     public function GeolocalisationAction()
     {
         return $this->render('@HealthAdvisor/Geolocalisation/index.html.twig');
+    }
+
+    public function AnalyseSymptAction()
+    {
+        return $this->render('HealthAdvisorBundle:Analyse_symptome:index.html.twig');
+    }
+
+    public function FindBodyPartsAction()
+    {
+
+        $bodyparts = $this->getDoctrine()->getRepository('HealthAdvisorBundle:BodyParts')->findAll();
+        $serializer = new Serializer(array(new ObjectNormalizer()));
+        $result = $serializer->normalize($bodyparts);
+        return new JsonResponse($result);
+
+    }
+
+    public function FindSubBodyPartsAction(Request $request)
+    {   $id=0;
+        if($request->isXmlHttpRequest()){
+           $id =$request->get('idbodypart');
+    }
+        $subbodyparts = $this->getDoctrine()->getRepository('HealthAdvisorBundle:SubBodyParts')->findBy(array('idBodyPart'=>$id));
+        $serializer = new Serializer(array(new ObjectNormalizer()));
+        $result = $serializer->normalize($subbodyparts);
+        return new JsonResponse($result);
+    }
+    public function FindSymptomsAction(Request $request)
+    {   $id=0;
+    $gender='';
+        if($request->isXmlHttpRequest()){
+           $id =$request->get('idsubbodypart');
+           $gender=$request->get('gender');
+    }
+        $symptoms = $this->getDoctrine()->getRepository('HealthAdvisorBundle:SubBodyPartsSymptome')->findBy(array('idSubBody'=>$id,'gender'=>$gender));
+        $serializer = new Serializer(array(new ObjectNormalizer()));
+        $result = $serializer->normalize($symptoms);
+        return new JsonResponse($result);
     }
 
 }
